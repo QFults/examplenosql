973be7534f328f49a536449f06d07b6be56a47ab
Quinton Fults <quinton.fults.work@gmail.com>
2023-01-12 13:34:22 -0800

    code

--- models/Food.js
+++ models/Food.js
@@ -1,10 +1,16 @@
 const { model, Schema } = require("mongoose");
 
+const ingredientSchema = new Schema({
+  name: { type: String, required: true },
+  quantity: Number
+})
+
 const Food = model(
   "Food",
   new Schema({
     name: { type: String, required: true },
     calories: Number,
+    ingredients: [ingredientSchema]
   })
 );
 
